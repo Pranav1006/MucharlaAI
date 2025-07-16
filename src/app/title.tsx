9638ae343fc03457900ae534efb39006bb4791d6
Pranav Mucharla <mucharlapranav@gmail.com>
2025-07-15 21:08:45 -0400

    Fixed the header on mobile and forced light mode

--- src/app/title.tsx
+++ src/app/title.tsx
@@ -1,9 +1,9 @@
 export default function Title() {
     return (
         <div className="flex justify-center items-center py-4">
-                <div className="text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 drop-shadow-lg text-center select-none">
-                    MUCHARLA AI
-                </div>
+            <div className="text-4xl sm:text-6xl md:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 drop-shadow-lg text-center select-none">
+            MUCHARLA AI
+            </div>
         </div>
     );
 }
